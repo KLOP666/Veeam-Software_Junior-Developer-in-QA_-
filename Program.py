29cd382a6e7b705da4dce1819e17f31fef3ab5bf
KLOP666 <86041418+KLOP666@users.noreply.github.com>
2021-11-14 15:02:34 +0300

    Add files via upload

--- Program.py
+++ Program.py
@@ -4,19 +4,19 @@ import psutil
 print("Time interval in minutes: ")
 Time_interval = float(input())*60
 f = open("data.txt", 'w')
-f.write("CPU(%): RSS(b): VMS(b): fd:\n")
+f.write("t(s): CPU(%): RSS(b): VMS(b): fd:\n")
 f.close()
-
-while time.time() < time.time() + Time_interval:
+start_time = time.time()
+while time.time() < start_time + Time_interval:
     # Начало процесса
     for i in range(0, 4999):
         a = i**i
     # Конец процесса
+    t = round(time.time() - start_time,2)
     CPU = psutil.cpu_percent(interval=None)
     process = psutil.Process(os.getpid())
     fd = process.num_fds()
-    print(fd)
-    line = [CPU, process.memory_info().rss, process.memory_info().vms, fd]
+    line = [t, CPU, process.memory_info().rss, process.memory_info().vms, fd]
     repr(line)
     with open("data.txt", 'a') as f:
         f.write(repr(line)+'\n')
